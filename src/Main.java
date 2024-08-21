7325f64d8a524963fa0aa2576e7b2365b9da0672
Arpit-7175 <bhardwajarpit7175@gmail.com>
2024-08-21 12:22:33 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        /*
+
+            SuggestionController controller=new SuggestionController();
+            methods in controller class
+            1. add(String) -> adds a string to the trie and updates its frequency in hashmap
+            2. addAll(List) -> adds multiple strings at once
+            3. suggest(String) -> returns a List of words that might be a potentil search for the user.
+            
+         */
     }
 }
